405e8213d6b474fb96a462901c93ff8a9fe981af
Rahul Rathore <rahul525230@gmail.com>
2023-07-27 15:47:14 +0530

    Initial commit

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,11 @@
+// import { CanActivateFn } from '@angular/router';
+// import { SellerService } from './services/seller.service';
+// import { Observable } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import { publishFacade } from '@angular/compiler';
+
+// export const authGuard: CanActivateFn = (route, state) => {
+//   constructor(public sellerService:SellerService){}
+//   return this.SellerService.isSellerLoggedIn;
+// };
+// // 
---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent {
+  constructor(private seller:SellerService, private router:Router) {}
+  signUp(data:object):void {
+    this.seller.userSignUp(data);
+  }
+}
